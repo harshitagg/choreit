2cbfa00909e63abb1d8b3b72b25c8db0129a7840
Harshit Agarwal <harshitagarwal90@gmail.com>
2014-03-12 02:37:48 -0400

    Harshit | Init project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Choreit::Application.config.secret_key_base = '8e755d82c19bc5e8b84451f3ab4fc084d8f7536502f236d3077802f068c06d790a8e93e7e961e2affb1bb5c3d588ca8d6c3298b6dca26044ff1990b0292f7034'
